e6aa102c4143cf1c9afece83b2ea8ac7be569d9e
HaHa <57859781+Viaje9@users.noreply.github.com>
2021-09-18 09:51:20 +0800

    新增reptile collection

---
+++ sqlite/reptile/collectionHistoryTMC.js
@@ -0,0 +1,34 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("../database/data.db");
+// const { getTMC } = require("./api/request");
+const {
+  delay,
+  formatQueryDate,
+  generateDateList,
+  selectItem
+} = require("../utils/utils");
+
+db.serialize(start);
+
+async function start() {
+  // db.run(
+  //   "CREATE TABLE IF NOT EXISTS stock (stockid TEXT,stockname TEXT,fib TEXT,fis TEXT,fit TEXT,itb TEXT,its TEXT,itt TEXT,seto TEXT,seob TEXT,seos TEXT,seot TEXT,sehb TEXT,sehs TEXT,seht TEXT,tmle TEXT,date TEXT)"
+  // );
+  const dateList = generateDateList();
+  for ({ queryTime, storeTime } of dateList) {
+    const data = await getTMC(queryTime);
+    if (data) {
+      const stmt = db.prepare(
+        "INSERT INTO stock(stockid,stockname,fib,fis,fit,itb,its,itt,seto,seob,seos,seot,sehb,sehs,seht,tmle,date) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+      );
+      data.forEach((e) => {
+        stmt.run(...selectItem(e), storeTime);
+      });
+      console.log(`${storeTime} done`);
+      stmt.finalize();
+    }
+    await delay(100, 1);
+  }
+}
+
+db.close()
