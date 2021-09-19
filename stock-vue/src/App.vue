966a66c8f22ea7b87dc0e4b4b3557af37f89d788
Viaje9 <57859781+Viaje9@users.noreply.github.com>
2021-09-19 16:06:00 +0800

    安裝lightweight-charts

--- stock-vue/src/App.vue
+++ stock-vue/src/App.vue
@@ -7,7 +7,13 @@
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  mounted() {
+    console.log(this);
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ stock-vue/src/plugins/lightweight-charts.js
@@ -0,0 +1,5 @@
+import { createChart } from 'lightweight-charts';
+const install = (Vue) => {
+  Object.defineProperty(Vue.prototype, '$createChart', { value: createChart });
+}
+export default install
