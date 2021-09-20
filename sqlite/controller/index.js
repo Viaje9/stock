9ddd1404ce32c40204b73bd607de22b37d958e9f
HaHa <57859781+Viaje9@users.noreply.github.com>
2021-09-20 20:24:04 +0800

    新增 category controller

--- sqlite/controller/index.js
+++ sqlite/controller/index.js
@@ -1,12 +1,14 @@
 const sqlite3 = require("sqlite3").verbose();
-const ISdb = new sqlite3.Database("./database/ISTable.db");
-const TMCdb = new sqlite3.Database("./database/TMCTable.db");
+const ISDb = new sqlite3.Database("./database/ISTable.db");
+const TMCBb = new sqlite3.Database("./database/TMCTable.db");
+const CategoryDb = new sqlite3.Database("./database/Category.db");
+
 
 class IndexController {
   static async getIS(req, res) {
     const stockId = req.query.id
     const data = await new Promise((resolve, reject) => {
-      ISdb.all(`SELECT * FROM ISTable where stockId='${stockId}'`, function (err, row) {
+      ISDb.all(`SELECT * FROM ISTable where stockId='${stockId}'`, function (err, row) {
         if (err) reject("QQ");
         resolve(row);
       });
@@ -16,7 +18,17 @@ class IndexController {
 
   static async getTMC(req, res) {
     const data = await new Promise((resolve, reject) => {
-      TMCdb.all(`SELECT * FROM TMCTable where stockId='${stockId}'`, function (err, row) {
+      TMCBb.all(`SELECT * FROM TMCTable where stockId='${stockId}'`, function (err, row) {
+        if (err) reject("QQ");
+        resolve(row);
+      });
+    });
+    res.send(data);
+  }
+
+  static async getMenu(req, res) {
+    const data = await new Promise((resolve, reject) => {
+      CategoryDb.all(`SELECT * FROM CategoryTable`, function (err, row) {
         if (err) reject("QQ");
         resolve(row);
       });
--- sqlite/router/index.js
+++ sqlite/router/index.js
@@ -3,5 +3,7 @@ const IndexController = require("../controller/index");
 
 router.get("/getIS", IndexController.getIS);
 router.get("/getTMC", IndexController.getTMC);
+router.get("/getMenu", IndexController.getMenu);
+
 
 module.exports = router
